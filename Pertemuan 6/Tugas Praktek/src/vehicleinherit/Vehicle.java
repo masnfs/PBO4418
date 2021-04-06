28b77c1baf5e732d5dac22a62ed3fa315f7ad967
masnfs <66139575+masnfs@users.noreply.github.com>
2021-04-06 07:58:34 +0700

    Pertemuan 6
    kumpulan tugas pertemuan 6

---
+++ Pertemuan 6/Tugas Praktek/src/vehicleinherit/Vehicle.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicleinherit;
+
+/**
+ *
+ * @author Ncy
+ */
+public class Vehicle {
+    public double speed = 0 ;
+    public String color = "orange";
+    
+    public Vehicle(){};
+    
+    public void goStraight(){
+        System.out.println("Maju..");
+    }
+    public void turnLeft(){
+        System.out.println("Belok Kiri");
+    }
+    public void turnRight(){
+        System.out.println("Belok Kanan");
+    }
+    
+}
