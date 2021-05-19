df4194fe6f2b5096ee8de7ce336ee7fcbe14c55b
masnfs <66139575+masnfs@users.noreply.github.com>
2021-05-19 13:35:12 +0700

    Pertemuan 10
    tugas pertemuan 10

---
+++ Pertemuan 10/Tugas Praktek/tugas10_praktek/src/tugas10_praktek/Tugas10_praktek.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas10_praktek;
+
+/**
+ *
+ * @author Ncy
+ */
+public class Tugas10_praktek {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
